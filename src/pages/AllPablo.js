5847aaed7312e5f4cb7ab20dbbba54e3f9d66c49
Lucgnl <lucasisgeek@gmail.com>
2022-03-30 18:35:05 +0100

    1

--- src/pages/AllPablo.js
+++ src/pages/AllPablo.js
@@ -5,7 +5,7 @@ function AllPablo()
     return (
         <section>
             <h1>
-                PABLO PICS ;)
+                PABLO PICS ;) NICE
             </h1>
             <PabloImg src='./pablo.jpg'/>
             <PabloImg src='https://static.wikia.nocookie.net/8235f47d-e93c-4969-ac47-56d57e62569f'/>
