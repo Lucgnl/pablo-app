05fbe3c22125113aa00bc52b30389fcfee9e17c6
Mark <70406421+Sneepex2@users.noreply.github.com>
2022-04-02 11:51:25 +0200

    Update Welcome.js

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -4,7 +4,7 @@ import classes from './Welcome.module.css';
 function Welcome() {
   return (
     <div className={classes.header}>
-      <h1>Welcome to Pablo's Weakly Interview</h1>
+      <h1>Welcome to Pablo's Weekly Interview</h1>
       <h2>Enjoy ur stay!</h2>
       <Link to='/all'>Go to All of Pablo</Link>
     </div>
